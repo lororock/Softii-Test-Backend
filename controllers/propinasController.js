ebc4f5d530a344604fe733115a1a49a10246702b
Cristhian Rosas <cristhianrosas159@hotmail.com>
2024-02-07 14:56:31 -0500

    first commit

---
+++ controllers/propinasController.js
@@ -0,0 +1,33 @@
+
+let totalPropinas = 0
+const efectivoCaja = 5500
+const metodoPago = 'tarjeta';
+const pagos = [{paymentType: 'EFECTIVO', VALUE: 4000, id: 1}]
+
+export const recuperarEfectivoCaja = (req, res) => {
+  // Implementar lógica para registrar la transacción en el sistema de punto de venta
+  res.json({ efectivoCaja });
+};
+
+// Controlador para capturar el monto total de propinas
+export const capturarPropinas = (req, res) => {
+  const { montoTotal } = req.body;
+  totalPropinas += montoTotal; // Sumar el monto total al total de propinas
+  res.json({ message: "Monto total de propinas capturado exitosamente" });
+};
+
+// Controlador para decidir la división de propinas
+export const dividirPropinas = (req, res) => {
+  const { dividir } = req.body;
+  // Implementar lógica para dividir las propinas entre los empleados
+  res.json({ message: "División de propinas realizada exitosamente" });
+};
+
+// Controlador para registrar el pago de propinas
+export const pagarPropinas = (req, res) => {
+  const { empleados } = req.body;
+  // Implementar lógica para registrar la transacción en el sistema de punto de venta
+  res.json({ message: "Pago de propinas registrado exitosamente" });
+};
+
+
---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import path from 'path';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import cors from "cors";
+import {
+  capturarPropinas,
+  dividirPropinas,
+  pagarPropinas,
+  recuperarEfectivoCaja,
+} from "./controllers/propinasController.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "frontend")));
+app.use(express.json());
+
+app.get("/efectivo-caja", recuperarEfectivoCaja);
+app.post("/propinas/capturar", capturarPropinas);
+app.post("/propinas/dividir", dividirPropinas);
+app.post("/propinas/pagar", pagarPropinas);
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
